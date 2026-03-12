1f0040576ab89405759cb45127964fd0934f2610
onur erkoç <onurbenena@gmail.com>
2026-03-12 19:53:11 +0300

    add ReactDOM root render with StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+// src/main.jsx dosyasında, React uygulamasını kök DOM elementine render ediyoruz. StrictMode, uygulamanın potansiyel sorunlarını tespit etmeye yardımcı olur.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
